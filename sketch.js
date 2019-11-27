9fb64eb26682fdc7e46ef32bf477aa010d3c39fb
andreas-dora <andreas.dora@icloud.com>
2019-11-27 02:22:27 +0100

    Layout korrigiert

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-sfunction tellTime() {
+function tellTime() {
     let now = new Date();
     let myH = now.getHours().toString().padStart(2, 0);
     let myM = now.getMinutes().toString().padStart(2, 0);
@@ -8,10 +8,10 @@ sfunction tellTime() {
     let myDate = now.getDate().toString().padStart(2, 0);
     // let myMonth = now.getMonth().toString().padStart(2, 0);
     let myMonth = now.getMonth();
-    let myY = now.getFullYear().toString();
+    // let myY = now.getFullYear().toString();
 
     const tage = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
-    const monate = ["Januar", "Februar", "März", "April", "Mai", "Juni", "August", "September", "Oktober", "November", "Dezember"];
+    // const monate = ["Januar", "Februar", "März", "April", "Mai", "Juni", "August", "September", "Oktober", "November", "Dezember"];
 
     let myTime = myH + ":" + myM; // ":" + myS;
     let myDatum = myDate + "." + myMonth +".";
