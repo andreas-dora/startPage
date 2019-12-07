0078704b4ea1096370cb81115c980fa4901a0c05
andreas-dora <andreas.dora@icloud.com>
2019-12-07 04:09:06 +0100

    Zeitfeld eingebaut

--- sketch.js
+++ sketch.js
@@ -1,27 +1,35 @@
+var volumi = document.getElementById("volumio");
+volumi.addEventListener("click", expander);
+var myH;
+function expander(){
+    window.alert("HELLO, WORLD " + myH);
+}
 
 function tellTime() {
     let now = new Date();
-    let myH = now.getHours().toString().padStart(2, 0);
+     myH = now.getHours().toString().padStart(2, 0);
     let myM = now.getMinutes().toString().padStart(2, 0);
  //   let myS = now.getSeconds().toString().padStart(2, 0);
 
     let myDay = now.getDay();
     let myDate = now.getDate().toString().padStart(2, 0);
-    // let myMonth = now.getMonth().toString().padStart(2, 0);
-    let myMonth = now.getMonth();
+    let myMonth = now.getMonth().toString().padStart(2, 0);
+    //let myMonth = now.getMonth();
     // let myY = now.getFullYear().toString();
 
     const tage = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
-    // const monate = ["Januar", "Februar", "März", "April", "Mai", "Juni", "August", "September", "Oktober", "November", "Dezember"];
+   const monate = ["Januar", "Februar", "März", "April", "Mai", "Juni", "August", "September", "Oktober", "November", "Dezember"];
 
     let myTime = myH + ":" + myM; // ":" + myS;
-    let myDatum = myDate + "." + myMonth +".";
-   //let myDatum = tage[myDay] + " " + myDate + ". " + monate[myMonth] + " " + myY;
+    //var myDatum = myDate;
+   // let myDatum = myDate + "." + myMonth +".";
+    var myDatum = tage[myDay] + " " + myDate + "." + myMonth;
+
 
     
-    document.getElementById("tag").innerText = tage[myDay];
+   // document.getElementById("tag").innerText = tage[myDay];
     document.getElementById("zeit").innerText = myTime;
     document.getElementById("datum").innerText = myDatum;
-w};
+}
 
 setInterval(tellTime, 1000);
