1fe059b23059c2712f120be2682fb6d335c909a7
andreas-dora <andreas.dora@icloud.com>
2020-08-04 15:53:02 +0200

    zwischenspeicher

--- pages/layouts/display/js/accordion.js
+++ pages/layouts/display/js/accordion.js
@@ -3,7 +3,7 @@ var i;
 
 for (i = 0; i < accord.length; i++) {
   accord[i].addEventListener("click", function() {
-    
+   
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     // panel.style.borderStyle = "none";
