113030e30113dd7527f281ea20a6d447197b1b99
andreas-dora <andreas.dora@icloud.com>
2021-06-16 17:18:29 +0200

    ladefehler

---
+++ calender/myframe.js
@@ -0,0 +1,7 @@
+const mf = document.getElementById('myFrame');
+const fbody = mf.contentWindow.document.getE
+fb
+
+// var iframe = document.getElementById("myFrame");
+// var elmnt = iframe.contentWindow.document.getElementsByTagName("button")[0];
+// elmnt.style.display = "none";
